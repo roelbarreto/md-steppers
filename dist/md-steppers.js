f1d1c591edb530f22075e9bdb0645cadaee1149d
Roel B <roel.mail73@gmail.com>
2016-02-04 01:27:39 +0800

    removed ink bar - need cleanup on controller for ink bar

--- dist/md-steppers.js
+++ dist/md-steppers.js
@@ -973,7 +973,7 @@ function MdSteppers() {
                         'md-scope="::step.parent" ',
                         '></md-step-label-wrapper>',
                     '</md-step-item> ',
-                  '<md-ink-bar></md-ink-bar> ' ,
+                  //'<md-ink-bar></md-ink-bar> ' ,
                   '</md-pagination-wrapper> ' ,
                   '<div class="md-visually-hidden md-dummy-wrapper"> ' ,
                     '<md-dummy-step ' ,
